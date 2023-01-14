990718fea6d1b171d182a6b762f56329caa90ff3
John Vresilovic <johnvresilovic@protonmail.com>
2023-01-14 18:45:02 -0500

    progress on conditional styling

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,14 +1,24 @@
+import React from "react"
+import styled from "styled-components"
+
+const StyledDiv = styled.div `
+  background-color: ${props => props.complete ? 'green' : 'red'};
+`
+
 const ListItem = ({element}) => {
     return (
-      <div className = 'each'>
-      <br/>
+      <>
+      <StyledDiv>
       <span>
     <ul><h4>{element.description}&nbsp;&nbsp;
     Completed  
       {element.complete === true ? 
-      (<input type="checkbox" checked></input>) : (<input type="checkbox"></input>)}</h4></ul>
+      (<input type="checkbox" checked></input>) : (<input type="checkbox"></input>)}
+      </h4></ul>
       </span>
-  </div>
+      </StyledDiv>
+      <br />
+  </>
     )
   }
   
