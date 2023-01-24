4021d42beaed39912269984356184b8d8f1f71b6
John Vresilovic <johnvresilovic@protonmail.com>
2023-01-24 14:52:33 -0500

    last one I hope

--- src/components/toDoItems.js
+++ src/components/toDoItems.js
@@ -1,39 +1,39 @@
-import { toDoItems, deleteItem } from '../services/Item-api'
+import { toDoItems, deleteItem } from "../services/Item-api";
 import { useState, useEffect } from "react";
-import Create from './CreateItem';
+import Create from "./CreateItem";
 import { Link } from "react-router-dom";
 
 export default function ToDoItems() {
-    const [todos, setTodos] = useState([])
-    useEffect(() => {
-        toDoItems() // calling the function to get the data
-        .then(res => setTodos(res.data)) // setting state with returned data
-    }, [])
-    const taskNotDone = {
-        backgroundColor: "yellow"
-    }
-    const taskDone = {
-        textDecoration: "line-through"
-    }
-   
-    return(
-        <div>
-            {todos.map((todo) => {
-                return (
-                    <div>
-                    <Link to={`/${todo._id}`}>{todo.description}</Link>
-                    </div>
-                )   
-            })}
-          <Create />  
-        </div>
-    )
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    toDoItems() // calling the function to get the data
+      .then((res) => setTodos(res.data)); // setting state with returned data
+  }, []);
+  const taskNotDone = {
+    backgroundColor: "yellow",
+  };
+  const taskDone = {
+    textDecoration: "line-through",
+  };
+
+  return (
+    <div>
+      {todos.map((todo) => {
+        if (todo.complete) {
+          return (
+            <p style={taskDone}>
+              <a href={`/${todo._id}`}>{todo.description}</a>
+            </p>
+          );
+        } else {
+          return (
+            <p style={taskNotDone}>
+              <a href={`/${todo._id}`}>{todo.description}</a>
+            </p>
+          );
+        }
+      })}
+      <Create />
+    </div>
+  );
 }
-
-
-// Old code do not delete
-// if (todo.complete) {
-//     return <p style={taskDone}><a href={`/${todo._id}`}>{todo.description}</a></p>
-// } else {
-//     return <p style={taskNotDone}><a href={`/${todo._id}`}>{todo.description}</a></p>
-// }
